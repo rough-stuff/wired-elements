f5c9eb297f162d5ed4c055c88704fa3c4838a974
Brian Kimball <bvkimball@gmail.com>
2019-07-05 10:26:57 -0400

    Add Dialog, Toast, & Table components for discussion

--- packages/all/src/wired-elements.ts
+++ packages/all/src/wired-elements.ts
@@ -2,6 +2,7 @@ export { WiredButton } from 'wired-button';
 export { WiredCard } from 'wired-card';
 export { WiredCheckbox } from 'wired-checkbox';
 export { WiredCombo } from 'wired-combo';
+export { WiredDialog } from 'wired-dialog';
 export { WiredIconButton } from 'wired-icon-button';
 export { WiredInput } from 'wired-input';
 export { WiredItem } from 'wired-item';
@@ -11,8 +12,16 @@ export { WiredRadio } from 'wired-radio';
 export { WiredRadioGroup } from 'wired-radio-group';
 export { WiredSlider } from 'wired-slider';
 export { WiredTextarea } from 'wired-textarea';
+export { WiredToast } from 'wired-toast';
 export { WiredToggle } from 'wired-toggle';
 export { WiredTooltip } from 'wired-tooltip';
+export {
+  WiredTable,
+  WiredColumns,
+  WiredColumn,
+  WiredRow,
+  WiredCell
+} from 'wired-table';
 export { WiredTab, WizardTabs } from 'wired-tabs';
 export { WiredFab } from 'wired-fab';
 export { WiredSpinner } from 'wired-spinner';
