5bf5bf68aead1f43a353dd65be40fa1b26dc255a
Preet Shihn <preetshihn@gmail.com>
2019-10-11 16:38:04 -0700

    wired-input impl

---
+++ src/wired-input.ts
@@ -0,0 +1,113 @@
+import { WiredBaseElement, BaseCSS } from './wired-base-element';
+import { customElement, property, query, css, TemplateResult, html, CSSResultArray } from 'lit-element';
+import { rectangle, Point, fire } from './core';
+
+@customElement('wired-input')
+export class WiredButton extends WiredBaseElement {
+  @property({ type: Boolean, reflect: true }) disabled = false;
+  @property({ type: String }) placeholder = '';
+  @property({ type: String }) name?: string;
+  @property({ type: String }) min?: string;
+  @property({ type: String }) max?: string;
+  @property({ type: String }) step?: string;
+  @property({ type: String }) type = 'text';
+  @property({ type: String }) autocomplete = '';
+  @property({ type: String }) autocapitalize = '';
+  @property({ type: String }) autocorrect = '';
+  @property({ type: Boolean }) required = false;
+  @property({ type: Boolean }) autofocus = false;
+  @property({ type: Boolean }) readonly = false;
+  @property({ type: Number }) minlength?: number;
+  @property({ type: Number }) maxlength?: number;
+  @property({ type: Number }) size?: number;
+
+  @query('input') private textInput?: HTMLInputElement;
+  private pendingValue?: string;
+
+  static get styles(): CSSResultArray {
+    return [
+      BaseCSS,
+      css`
+        :host {
+          display: inline-block;
+          position: relative;
+          padding: 5px;
+          font-family: sans-serif;
+          width: 150px;
+          outline: none;
+        }
+        :host([disabled]) {
+          opacity: 0.6 !important;
+          cursor: default;
+          pointer-events: none;
+        }
+        :host([disabled]) svg {
+          background: rgba(0, 0, 0, 0.07);
+        }
+        input {
+          display: block;
+          width: 100%;
+          box-sizing: border-box;
+          outline: none;
+          border: none;
+          font-family: inherit;
+          font-size: inherit;
+          font-weight: inherit;
+          color: inherit;
+          padding: 6px;
+        }
+      `
+    ];
+  }
+
+  render(): TemplateResult {
+    return html`
+    <input name="${this.name}" type="${this.type}" placeholder="${this.placeholder}" ?disabled="${this.disabled}"
+      ?required="${this.required}" autocomplete="${this.autocomplete}" ?autofocus="${this.autofocus}" minlength="${this.minlength}"
+      maxlength="${this.maxlength}" min="${this.min}" max="${this.max}" step="${this.step}" ?readonly="${this.readonly}"
+      size="${this.size}" autocapitalize="${this.autocapitalize}" autocorrect="${this.autocorrect}" @change="${this.onChange}">
+    <div id="overlay">
+      <svg></svg>
+    </div>
+    `;
+  }
+
+  get input(): HTMLInputElement | undefined {
+    return this.textInput;
+  }
+
+  get value(): string {
+    const input = this.input;
+    return (input && input.value) || '';
+  }
+
+  set value(v: string) {
+    if (this.shadowRoot) {
+      const input = this.input;
+      if (input) {
+        input.value = v;
+      }
+    } else {
+      this.pendingValue = v;
+    }
+  }
+
+  firstUpdated() {
+    this.value = this.pendingValue || this.value || this.getAttribute('value') || '';
+    delete this.pendingValue;
+  }
+
+  protected canvasSize(): Point {
+    const s = this.getBoundingClientRect();
+    return [s.width, s.height];
+  }
+
+  protected draw(svg: SVGSVGElement, size: Point) {
+    rectangle(svg, 2, 2, size[0] - 2, size[1] - 2);
+  }
+
+  private onChange(event: Event) {
+    event.stopPropagation();
+    fire(this, event.type, { sourceEvent: event });
+  }
+}
