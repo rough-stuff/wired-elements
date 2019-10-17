4985f242a9bfd4fa865640718f17fcc73a4c81f0
Preet Shihn <preetshihn@gmail.com>
2019-10-17 15:37:05 -0700

    volume icon

--- packages/wired-video/src/wired-video.ts
+++ packages/wired-video/src/wired-video.ts
@@ -137,6 +137,9 @@ export class WiredVideo extends WiredBase {
         <div class="flex">
           <wired-slider></wired-slider>
         </div>
+        <div style="width: 24px; height: 24px;">
+          <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g><path style="stroke: none; fill: currentColor;" d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path></g></svg>
+        </div>
       </div>
     </div>
     `;
