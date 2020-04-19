34e723a6e25665f7ed0a8407c3c1fbcd96f32cec
apennamen <adrien.pennamen@soprasteria.com>
2020-04-19 23:52:57 +0200

    feat: include wired-icon to wired-elements

--- packages/all/src/wired-elements.ts
+++ packages/all/src/wired-elements.ts
@@ -7,6 +7,7 @@ export { WiredDialog } from 'wired-dialog';
 export { WiredDivider } from 'wired-divider';
 export { WiredFab } from 'wired-fab';
 export { WiredIcon } from 'wired-icon';
+export { iconsetLoader } from 'wired-icon/lib/iconset'
 export { WiredIconButton } from 'wired-icon-button';
 export { WiredImage } from 'wired-image';
 export { WiredInput } from 'wired-input';
