b2771613a4c6b4d56ae57a8a25c924e1735bd437
apennamen <adrien.pennamen@soprasteria.com>
2020-04-25 11:46:32 +0200

    fix: more elegant solution to customRegistry pb

--- packages/wired-icon/src/wired-icon.ts
+++ packages/wired-icon/src/wired-icon.ts
@@ -4,5 +4,6 @@ import { WiredIcon as Base } from './WiredIcon';
 // We separate the class from its registration as a custom element,
 // so that WiredIcon class can be extended.
 // Otherwise, CustomElementRegistry would register it twice and would throw an error.
-@customElement('wired-icon')
-export class WiredIcon extends Base {};
+const WiredIcon = customElement('wired-icon')(Base);
+
+export { WiredIcon };
