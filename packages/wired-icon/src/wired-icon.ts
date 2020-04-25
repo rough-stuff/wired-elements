5841c16d76208901f6a9c565ed0ec12aaaa28459
apennamen <adrien.pennamen@soprasteria.com>
2020-04-25 11:58:46 +0200

    fix: right export for wired-icon

--- packages/wired-icon/src/wired-icon.ts
+++ packages/wired-icon/src/wired-icon.ts
@@ -1,9 +1,9 @@
 import { customElement } from 'lit-element';
-import { WiredIcon as Base } from './WiredIcon';
+import { WiredIcon } from './WiredIcon';
 
 // We separate the class from its registration as a custom element,
 // so that WiredIcon class can be extended.
 // Otherwise, CustomElementRegistry would register it twice and would throw an error.
-const WiredIcon = customElement('wired-icon')(Base);
+customElement('wired-icon')(WiredIcon);
 
 export { WiredIcon };
